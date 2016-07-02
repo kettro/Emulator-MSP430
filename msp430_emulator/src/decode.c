fa6d6de394e1c67cefc5e1b98f72053a949ef9ab
drevyek <rkettleson@gmail.com>
2016-07-02 02:52:01 -0300

    added skeleton to decode.c

--- msp430_emulator/src/decode.c
+++ msp430_emulator/src/decode.c
@@ -1,13 +1,25 @@
 // Author : Ross Kettleson
-// File: 
-// Purpose: 
+// File: decode.c
+// Purpose: Contains functions to interpret the incoming instructions
 
 // Include Files
+#include <stdint.h>
+#include "structures.h"
+
 // Defines and Macros
 // Local Variables
 // Local Function Prototypes
+void parseOpcode(record_t record);
+void parseOperand(record_t record);
+void calculateOffset(record_t record);
+void calculateOperandAddress(record_t record);
 // External Variables
+extern uint16_t A_x;
+extern uint16_t B_x;
+extern uint16_t MAB_x;
+extern uint16_t MDB_x;
 // External Function Prototypes
+extern int alu(record_t record, status_reg_t* sr);
 
 // Definitions
 
