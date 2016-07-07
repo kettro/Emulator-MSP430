b59c4af0274f2dc0257133e6731af8b8651fd71f
drevyek <rkettleson@gmail.com>
2016-07-07 14:15:23 -0300

    adding static to lt's, adding 2 new lt's, small macro () edit

--- msp430_emulator/inc/lookup_tables.h
+++ msp430_emulator/inc/lookup_tables.h
@@ -7,7 +7,7 @@
 
 #include "enums.h"
 
-MathShiftLogic_e MathShiftLogic_lt[16] = { // opcode_type, opcode
+static MathShiftLogic_e MathShiftLogic_lt[16] = { // opcode_type, opcode
   // Type 2 opcodes start at 0x04 with MOV, which is not in the ALU
   UNDEFINED_msl,
   UNDEFINED_msl,
@@ -27,7 +27,7 @@ MathShiftLogic_e MathShiftLogic_lt[16] = { // opcode_type, opcode
   LOGIC_msl
 };
 
-int Carry_lt[16] = {
+static int Carry_lt[16] = {
   // Uses a carry? 0 = no, 1 = yes
   0,
   0,
@@ -47,7 +47,7 @@ int Carry_lt[16] = {
   0
 };
 
-int NegSub_lt[16] = {
+static int NegSub_lt[16] = {
   // needs to be negated? 
   0,
   0,
@@ -67,7 +67,7 @@ int NegSub_lt[16] = {
   0
 };
 
-LogicOperation_e LogicOperation_lt[16]= {
+static LogicOperation_e LogicOperation_lt[16]= {
 // 0 = UNDEFINED_lo
   0,
   0,
@@ -85,6 +85,27 @@ LogicOperation_e LogicOperation_lt[16]= {
   OR_lo,  // BIS
   XOR_lo, // XOR
   AND_lo  // AND
+};
+
+static char ConstantGeneratorAsReg[4][4] = {
+  // only from CG1 and CG2 = R3, R2
+  // if -2: error: not possible
+  {-2,-2,-2,0}, // As = 0
+  {-2,-2,-2,1}, // As = 1
+  {-2,-2,4,2}, // As = 2
+  {-2,-2,8,-1}  // As = 3
+};
+
+static AddrMode_e GetAddrMode_lt[4][16] = {
+  // 0 = REG_DIR
+  // 1 = indexed, relative, or absolute
+  // 2 = indirect
+  // 3 = indirect_aa
+  // 4 = immediate; Immediate iff Using SR/R3, with As: otherwise, it is the std value
+  {REG_DIR,REG_DIR,REG_DIR,IMM_CG,REG_DIR,REG_DIR,REG_DIR,REG_DIR,REG_DIR,REG_DIR,REG_DIR,REG_DIR,REG_DIR,REG_DIR,REG_DIR,REG_DIR},
+  {RELATIVE,INDEXED,ABSOLUTE,IMM_CG,INDEXED,INDEXED,INDEXED,INDEXED,INDEXED,INDEXED,INDEXED,INDEXED,INDEXED,INDEXED,INDEXED,INDEXED},
+  {INDIRECT,INDIRECT,IMM_CG,IMM_CG,INDIRECT,INDIRECT,INDIRECT,INDIRECT,INDIRECT,INDIRECT,INDIRECT,INDIRECT,INDIRECT,INDIRECT,INDIRECT,INDIRECT},
+  {IMMEDIATE,INDIRECT_AA,IMM_CG,IMM_CG,INDIRECT_AA,INDIRECT_AA,INDIRECT_AA,INDIRECT_AA,INDIRECT_AA,INDIRECT_AA,INDIRECT_AA,INDIRECT_AA,INDIRECT_AA,INDIRECT_AA,INDIRECT_AA,INDIRECT_AA}
 };
 
 
--- msp430_emulator/inc/macros.h
+++ msp430_emulator/inc/macros.h
@@ -6,15 +6,16 @@
 #define MACROS_H
 
 // To find the Sign bit of a Byte
-#define BIT7(word)            ( (word >> 7) & 0x0001 )
-#define BIT15(word)           ( ( word >> 15) & 0x0001 )
+#define BIT7(word)            ( ((word) >> 7) & 0x0001 )
+#define BIT15(word)           ( ((word) >> 15) & 0x0001 )
+
 
 // To Mask a Byte or word
-#define BYTEMASK(word)        ( (word & 0xFF) )
-#define WORDMASK(word)        ( (word & 0xFFFF) )
+#define BYTEMASK(word)        ( ((word) & 0xFF) )
+#define WORDMASK(word)        ( ((word) & 0xFFFF) )
 
 // To Mask the MSByte and LSByte of a word
-#define LSBYTE(word)          ( (word & 0x00FF) )
-#define MSBYTE(word)          ( (word >> 8) & 0x00FF);
+#define LSBYTE(word)          ( ((word) & 0x00FF) )
+#define MSBYTE(word)          ( ((word) >> 8) & 0x00FF);
 
 #endif
